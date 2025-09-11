aa91fcd5f36660384cd177e8ba3ee3a9c3b8c38e
Молоканов Никита <Tyuiu.molokanovNK@outlook.com>
2025-09-11 21:39:20 +0500

    Task7

---
+++ Tyuiu.MolokanovNK.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MolokanovNK.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MolokanovNK.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c0e7746-4c9f-4852-bba8-6afa822ca802")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MolokanovNK.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MolokanovNK.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.MolokanovNK.Sprint0.Task7.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнил : Молоканов Н. К. | ИБКСб-25-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Молоканов Никита Константинович | ИБКСб-25-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значение двух    *");
+            Console.WriteLine("* Одинаковых массивов по длине.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            //Место решения задания
+            int[] arraysNums1 = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Значение элементов массива №1");
+            for (int i = 0; i < arraysNums1.Length; i++)
+            {
+                Console.WriteLine(arraysNums1[i] + ", ");
+            }
+
+            Console.WriteLine();
+            
+            int[] arraysNums2 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Значение элементов массива №2");
+            for(int i = 0; i < arraysNums2.Length;i++)
+            {
+                Console.WriteLine(arraysNums2[i] + ", ");   
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (arraysNums1.Length == arraysNums2.Length)
+            {
+                int[] resultArray = DataService.AdditionArrays(arraysNums1, arraysNums2);
+
+                Console.WriteLine("Сумма элементов массива ровна:");
+                for (int i = 0; i < resultArray.Length;i++)
+                {
+                    Console.WriteLine(resultArray[i] + ", ");
+                }
+            }
+             else
+            {
+                Console.WriteLine("Ошибка! Исходные массивы имеют разное количество элементов");
+            }
+            Console.ReadKey();
+        }
+    }
+}
